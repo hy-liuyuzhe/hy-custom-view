b63f9c596cfdd2c68fce2a618bb2ea42a5ce8bfb
liuyuzhe <>
2021-12-23 10:23:00 +0800

    【需求: Android】【滚动动画处理数据++】
    Signed-off-by: liuyuzhe <>

--- app/src/main/java/com/yuwq/hy_learn_draw/RollTextActivity.kt
+++ app/src/main/java/com/yuwq/hy_learn_draw/RollTextActivity.kt
@@ -2,8 +2,10 @@ package com.yuwq.hy_learn_draw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.yuwq.hy_learn_draw.roll.RollNumberLayout
+import kotlin.random.Random
 
 class RollTextActivity : AppCompatActivity() {
     lateinit var layoutRollNumber: RollNumberLayout
@@ -14,13 +16,25 @@ class RollTextActivity : AppCompatActivity() {
     }
 
     fun plus(view: View) {
-        val n = layoutRollNumber.currentTextView.text.toString().toInt()
-        layoutRollNumber.setValue(n + 10)
+        val n = layoutRollNumber.lastDesiredValue.toString().toInt()
+        val finalN = n + Random.nextInt(10)
+        Log.d("liuyuzhe", "finalN: $finalN");
+        layoutRollNumber.setValue(finalN)
     }
 
     fun minus(view: View) {
-        val n = layoutRollNumber.currentTextView.text.toString().toInt()
-        layoutRollNumber.setValue(n - 10)
+        val n = layoutRollNumber.lastDesiredValue.toString().toInt()
+        layoutRollNumber.setValue(n - Random.nextInt(10))
+    }
+
+    var desiredValue = 2000
+    fun plus2(view: View) {
+        if (desiredValue>0){
+            desiredValue = -100
+        }else{
+            desiredValue = 1000
+        }
+        layoutRollNumber.setValue(desiredValue)
     }
 
 
--- app/src/main/java/com/yuwq/hy_learn_draw/roll/RollNumberLayout.kt
+++ app/src/main/java/com/yuwq/hy_learn_draw/roll/RollNumberLayout.kt
@@ -1,9 +1,11 @@
 package com.yuwq.hy_learn_draw.roll
 
-import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
 import android.widget.FrameLayout
 import android.widget.TextView
 import com.yuwq.hy_learn_draw.R
@@ -15,10 +17,11 @@ class RollNumberLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : FrameLayout(context, attrs) {
 
-    private var lastDesiredValue: Int = 0
-    var nextTextView: TextView
+    private lateinit var translateAnimation: Animation
+    var lastDesiredValue: Int = 0
+    private var nextTextView: TextView
     var currentTextView: TextView
-    val DURATION = 200L
+    private val DURATION = 250L
 
     init {
         inflate(context, R.layout.layout_roll_number, this)
@@ -26,10 +29,27 @@ class RollNumberLayout @JvmOverloads constructor(
         currentTextView = this.findViewById<TextView>(R.id.currentTextView)
 
         nextTextView.translationY = height.toFloat()
-        setValue(100)
+        setValue(0)
     }
 
     fun setValue(desiredValue: Int) {
+        Log.d("liuyuzhe", "desired: $desiredValue");
+        if (this::translateAnimation.isInitialized && !translateAnimation.hasEnded()) {
+            Log.d("liuyuzhe", "点的过快: $lastDesiredValue");
+            translateAnimation.setAnimationListener(null)
+            translateAnimation.cancel()
+            translateAnimation.reset()
+            currentTextView.text = lastDesiredValue.toString()
+            nextTextView.text = lastDesiredValue.toString()
+        }
+
+        if (Math.abs(desiredValue - lastDesiredValue) > 10) {
+            val fixedContent = if (desiredValue > lastDesiredValue) (desiredValue - 10).toString() else (desiredValue + 10).toString()
+            Log.d("liuyuzhe", "fixedContent: $fixedContent");
+            currentTextView.text = fixedContent
+            nextTextView.text = fixedContent
+        }
+
         this.lastDesiredValue = desiredValue
         if (currentTextView.text.isNullOrEmpty()) {
             currentTextView.text = desiredValue.toString()
@@ -41,35 +61,50 @@ class RollNumberLayout @JvmOverloads constructor(
             //current is minus to hide
             currentTextView.animate().translationY(-height.toFloat()).setDuration(DURATION).start()
             //next is plus to run the animation
-            nextTextView.translationY = nextTextView.height.toFloat()
-            nextTextView.animate().translationY(0f).setDuration(DURATION)
-                .setListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator?) {
-                        super.onAnimationEnd(animation)
-                        currentTextView.text = (oldValue - 1).toString()
-                        currentTextView.translationY = 0f
-                        if (oldValue + 1 != desiredValue) {
-                            setValue(desiredValue)
-                        }
+            translateAnimation = TranslateAnimation(0f, 0f, nextTextView.height.toFloat(), 0f)
+            translateAnimation.duration = DURATION
+            translateAnimation.setAnimationListener(object : AnimatorListenerAdapter(),
+                Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {
+                }
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    currentTextView.text = (oldValue - 1).toString()
+                    currentTextView.translationY = 0f
+                    if (oldValue + 1 != desiredValue) {
+                        setValue(desiredValue)
                     }
-                }).start()
+                }
+
+                override fun onAnimationRepeat(animation: Animation?) {
+                }
+            })
+            translateAnimation.start()
+            nextTextView.animation = translateAnimation
         } else if (oldValue < desiredValue) {
             nextTextView.text = (oldValue + 1).toString()
 
             currentTextView.animate().translationY(height.toFloat()).setDuration(DURATION).start()
+            translateAnimation = TranslateAnimation(0f, 0f, -nextTextView.height.toFloat(), 0f)
+            translateAnimation.duration = DURATION
+            translateAnimation.setAnimationListener(object : AnimatorListenerAdapter(),
+                Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {
+                }
 
-            nextTextView.translationY = -nextTextView.height.toFloat()
-            nextTextView.animate().translationY(0f).setDuration(DURATION)
-                .setListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator?) {
-                        super.onAnimationEnd(animation)
-                        currentTextView.text = (oldValue + 1).toString()
-                        currentTextView.translationY = 0f
-                        if (oldValue + 1 != desiredValue) {
-                            setValue(desiredValue)
-                        }
+                override fun onAnimationEnd(animation: Animation?) {
+                    currentTextView.text = (oldValue + 1).toString()
+                    currentTextView.translationY = 0f
+                    if (oldValue + 1 != desiredValue) {
+                        setValue(desiredValue)
                     }
-                }).start()
+                }
+
+                override fun onAnimationRepeat(animation: Animation?) {
+                }
+            })
+            translateAnimation.start()
+            nextTextView.animation = translateAnimation
         }
     }
 }
